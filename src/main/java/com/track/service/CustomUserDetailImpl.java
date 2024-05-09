989ac7d1645cd8e6ecd4c5608c99ff13091ab96b
viveksahu06 <viveksahu1006@gmail.com>
2024-05-09 10:48:39 +0530

    data

---
+++ src/main/java/com/track/service/CustomUserDetailImpl.java
@@ -0,0 +1,30 @@
+package com.track.service;
+
+import com.track.model.User;
+import com.track.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomUserDetailImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=userRepository.findByEmail(username);
+        if(user==null) {
+            throw new UsernameNotFoundException("user not found by email "+username);
+        }
+        List<GrantedAuthority>authorities=new ArrayList<>();
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(),authorities);
+    }
+}
