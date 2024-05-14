b5eec33dadaa80409fe5b3fa08a57729b6fd9ff4
Vishanu <vishanugiri2002@gmail.com>
2024-05-14 18:18:35 +0530

    react added

---
+++ react/project-management-react/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProjectList from "./ProjectList/ProjectList";
+
+const Home = () =>{
+    return(
+        <div>
+            <ProjectList/>
+        </div>
+    )
+}
+
+export default Home
