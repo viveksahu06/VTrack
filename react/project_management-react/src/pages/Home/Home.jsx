530d4513165751453b70279b757d285b2be44ca9
Vishanu <vishanugiri2002@gmail.com>
2024-05-17 17:35:45 +0530

    Bug fixed moving next part

---
+++ react/project_management-react/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProjectList from '../ProjectList/ProjectList'
+
+function Home() {
+    return (
+        <div>
+            <ProjectList />
+        </div>
+    )
+}
+
+export default Home
---
+++ react/project_management-react/src/pages/ProjectList/ProjectList.jsx
@@ -0,0 +1,98 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { MixerHorizontalIcon } from "@radix-ui/react-icons";
+
+import React from "react";
+
+const tags = [
+  "all",
+  "react",
+  "nextjs",
+  "spring boot",
+  "mysql",
+  "mangodb",
+  "angular",
+  "python",
+  "flask",
+  "django",
+];
+
+const ProjectList = () => {
+  const handleFilterChange = (value) => {
+    console.log("value", value);
+  };
+  return (
+    <>
+      <div className="relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5">
+        <section className="filterSection">
+          <Card className="p-5 sticky top-10">
+            <div className="flex justify-between lg:w-[20rem]">
+              <p className="text-xl -tracking-wider">filters</p>
+              <Button variant="ghost" size="icon">
+                <MixerHorizontalIcon />
+              </Button>
+            </div>
+            <CardContent className="mt-5">
+              <ScrollArea className="space-y-7 h-[70vh]">
+                <div>
+                  <h1 className="pb-3 text-gray-400 border-b">category</h1>
+                  <div className="pt-5">
+                    <RadioGroup
+                      defaultValue="all"
+                      onValueChange={(value) =>
+                        handleFilterChange("category", value)
+                      }
+                    >
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="all" id="r1" />
+                        <Label htmlFor="r1">all</Label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="fullstack" id="r2" />
+                        <Label htmlFor="r2">fullstack</Label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="frontend" id="r3" />
+                        <Label htmlFor="r3">frontend</Label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="backend" id="r4" />
+                        <Label htmlFor="r4">backend</Label>
+                      </div>
+                    </RadioGroup>
+                  </div>
+                </div>
+
+                <div className="pt-9">
+                  <h1 className="pb-3 text-gray-400 border-b">Tag</h1>
+                  <div className="pt-5">
+                    <RadioGroup
+                      className="space-y-3 pt-5"
+                      defaultValue="all"
+                      onValueChange={(value) =>
+                        handleFilterChange("tag", value)
+                      }
+                    >
+                      {tags.map((item) => (
+                        <div key={item} className="flex items-center gap-2">
+                          <RadioGroupItem value={item} id={`r1-${item}`} />
+                          <Label htmlFor={`r1-${item}`}>{item}</Label>
+                        </div>
+                      ))}
+                    </RadioGroup>
+                  </div>
+                </div>
+              </ScrollArea>
+            </CardContent>
+          </Card>
+        </section>
+        <section className="projectListSection w-full lg:w-[48rem]"></section>
+      </div>
+    </>
+  );
+};
+
+export default ProjectList;
